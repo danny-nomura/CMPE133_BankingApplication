7b4a8630a827c5a58069338cc2575fdbedaf266a
dkiskamp <47258708+dkiskamp@users.noreply.github.com>
2020-12-08 16:35:09 -0800

    Fixed heroku fragment

--- Bankit/Bankit/settings.py
+++ Bankit/Bankit/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,5 +127,3 @@ STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-
-django_heroku.settings(locals())
--- README.md
+++ README.md
@@ -3,6 +3,8 @@ Online Banking application for CMPE133 Final Project
 
 ### Running the application
 
-Simply run startserver.bat.
+Ensure you have django installed with `pip install django`
+
+Then run startserver.bat.
 
 To connect go to localhost:8000 or 127.0.0.1:8000 in your preferred web browser.
